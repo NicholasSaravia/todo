c2a8e914ba2678ce13313bf87706936569175678
Nicholas <nick.saravia@gmail.com>
2023-07-03 13:30:52 -0400

    ProgressBar component

---
+++ components/ProgressBar.tsx
@@ -0,0 +1,7 @@
+export const ProgressBar = () => {
+  return (
+    <div className="rounded-full border border-white h-3 w-[50px] -rotate-90">
+      <div className="h-full bg-white rounded-full w-[80%]"></div>
+    </div>
+  );
+};
